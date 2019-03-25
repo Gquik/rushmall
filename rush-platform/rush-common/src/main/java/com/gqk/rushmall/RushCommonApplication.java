a021e0b23ab13ec5222a1fbac007b6bca2ee26a4
gquik <gquik@qq.com>
2019-03-25 14:22:05 +0800

    尝试提交

---
+++ rush-platform/rush-common/src/main/java/com/gqk/rushmall/RushCommonApplication.java
@@ -0,0 +1,13 @@
+package com.gqk.rushmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RushCommonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RushCommonApplication.class, args);
+	}
+
+}
---
+++ rush-platform/rush-config/src/main/java/com/gqk/rushmall/RushConfigApplication.java
@@ -0,0 +1,13 @@
+package com.gqk.rushmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RushConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RushConfigApplication.class, args);
+	}
+
+}
